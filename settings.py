1a14ddf87ec69dc153dcc285e10f0bc6861aa7f9
Dmitry Bashkatov <nailgunster@gmail.com>
2009-11-30 16:43:12 +0300

    fix settings module

--- settings.py
+++ settings.py
@@ -5,10 +5,10 @@ JS_PREFIX = getattr(settings, 'JS_PREFIX', 'js/')
 CSS_PREFIX = getattr(settings, 'CSS_PREFIX', 'css/')
 
 def dj_media(filename):
-	return '%s/%s' % (DJ_URL, filename)
+	return ''.join((DJ_URL, filename))
 
 def js_media(filename):
-	return '%s/%s' % (JS_PREFIX, filename)
+	return ''.join((JS_PREFIX, filename))
 
 def css_media(filename):
-	return '%s/%s' % (CSS_PREFIX, filename)
+	return ''.join((CSS_PREFIX, filename))
